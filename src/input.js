de0671db1f86af4ae42a26a5f9329bbac5e792d7
Aviral Jain <jainaviral2002@gmail.com>
2022-08-06 12:58:23 +0530

    databse

--- src/input.js
+++ src/input.js
@@ -2,10 +2,11 @@ import React from "react";
 import './input.css';
 const encodedParams = new URLSearchParams();
 const axios = require("axios");
+const button = document.getElementById('myButton');
 
 function Input() {
-    var text=document.getElementById("textarea").value;
-    var date=document.getElementById("date")
+    var text=document.getElementById("textarea")?.value || ''
+    var date=document.getElementById("date");
     var time=document.getElementById("time")
     encodedParams.append("q",text);
 encodedParams.append("target", "hi");
@@ -20,9 +21,20 @@ encodedParams.append("source", "en");
       },
       data: encodedParams
     };
+    if (button){
+    button.addEventListener('click', async _=>  {
+     const add= await fetch('http://localhost:5000/record/add',{
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify( date, time),
+   })})};
+ 
+
     
     // axios.request(options).then(function (response) {
-    //   console.log(response.data);
+    //   var resp= response.data;
     // }).catch(function (error) {
     //   console.error(error);
     // });
